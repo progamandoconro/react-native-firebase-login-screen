6849132f2cf573ecf26084486ee4974a74394ab8
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-11-06 10:38:40 +0000

    Update config.js

--- src/core/config.js
+++ src/core/config.js
@@ -1,11 +1,5 @@
 // Replace with your own firebase config!
 export const FIREBASE_CONFIG = {
-apiKey: "AIzaSyDUcUtSFnsIoZi9GGPku6Wd9fegr1n4K8w",
-    authDomain: "test-38cf3.firebaseapp.com",
-    databaseURL: "https://test-38cf3.firebaseio.com",
-    projectId: "test-38cf3",
-    storageBucket: "test-38cf3.appspot.com",
-    messagingSenderId: "915006966267",
-    appId: "1:915006966267:web:c300898b1b2cbd3e8fe35e"
+
 
 };
